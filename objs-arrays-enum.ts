63f9a57f3933a89bbef81693c743e2f186f0acfa
tato jakhua <anatoli.jakhua@gmail.com>
2023-12-02 09:06:09 -0800

    Getting Started && TypeScript Basics & Basi Types

---
+++ objs-arrays-enum.ts
@@ -0,0 +1,48 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Tato",
+//   age: 26,
+//   hobbies: ["Coding", "Cooking"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = "ADMIN",
+  READ_ONLY = 100,
+  AUTHOR = "ADMIN",
+}
+
+const person = {
+  name: "Tato",
+  age: 26,
+  hobbies: ["Coding", "Cooking"],
+  role: Role.ADMIN,
+};
+
+// person.role.push("admin");
+// person.role[1] = 10;
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("is admin");
+}
