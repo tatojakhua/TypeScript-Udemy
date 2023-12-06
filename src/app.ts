f958ca0ad890724d8a59f4d859b5c2602e956640
tato jakhua <anatoli.jakhua@gmail.com>
2023-12-06 04:12:03 -0800

    Understanding The TypeScript Compiler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-let appId = 'abc';
-const button = document.querySelector('button')!;
+let appId = "abc";
+const button = document.querySelector("button")!;
 
 function add(n1: number, n2: number) {
   if (n1 + n2 > 0) {
@@ -10,9 +10,9 @@ function add(n1: number, n2: number) {
 
 function clickHandler(message: string) {
   // let userName = 'Max';
-  console.log('Clicked! ' + message);
+  console.log("Clicked! " + message);
 }
 // a comment
 if (button) {
-  button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
+  button.addEventListener("click", clickHandler.bind(null, "You're welcome!"));
 }
