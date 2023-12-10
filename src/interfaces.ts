f738ae692e7f5c2b5ef869c92223bce99cf11d15
tato jakhua <anatoli.jakhua@gmail.com>
2023-12-10 05:41:05 -0800

    Classes & Interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,44 @@
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => n1 + n2;
+
+interface Named {
+  readonly name?: string;
+  outputName?: string;
+}
+
+interface Greetable extends Named {
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name?: string;
+  age = 26;
+
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
+  }
+
+  greet(phrase: string) {
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("Hi!");
+    }
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person();
+// user1.name = "Manu";
+
+user1.greet("Hi there ~ I`m");
+console.log(user1);
